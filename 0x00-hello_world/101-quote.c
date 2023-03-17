b6924f4b07124f0a7bb0e190c143d7fe3aac456f
imamyah <imanemachkour8@gmail.com>
2023-03-17 00:03:21 +0000

    C program that prints a string with putchar() function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 1(Success)
+ * Return: Always 1 (Success) 
  */
 
 int main(void)
 {
-	char *c="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *c = " and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
 		putchar(*c);
 		return (1);
 
