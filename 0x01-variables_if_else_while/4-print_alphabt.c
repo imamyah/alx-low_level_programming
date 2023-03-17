13f54c4cb6a6c4f0f267ee01e1ba9416bb2cce31
imamyah <imanemachkour8@gmail.com>
2023-03-17 23:26:15 +0000

    c program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, exept q and e,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while(ch <= 'z')
+	{
+		if(ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base
+ * starting from 0 to 10, followed with a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 0; n <= 9; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
