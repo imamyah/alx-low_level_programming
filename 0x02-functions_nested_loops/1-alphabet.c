c2d2f2dfaf7f00e0d39d62a6902720400e7b905b
imamyah <imanemachkour8@gmail.com>
2023-03-21 22:39:12 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ * @ch: The character to print
+ * Return: On success 1
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+	for( ch = 'a'; ch <= 'z'; ch++ )
+		_putchar(ch);
+	_putchar('\n');
+}
