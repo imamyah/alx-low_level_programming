e57a477e68067b50a90052c24b06889fc9bd45b6
imamyah <imanemachkour8@gmail.com>
2023-03-23 22:45:00 +0000

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,13 +3,10 @@
 /**
  * print_line - draws a straight line
  * @n: The number of time _ should be printed
- * Return: Always 0 (Success)
  */
 
 void print_line(int n)
 {
-	char c = '_';
-
 	if (n <= 0)
 	{
 		_putchar('\n');
