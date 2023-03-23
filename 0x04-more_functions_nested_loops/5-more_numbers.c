8f2990ffd3698016a7e3c5957bca3f43856c7ab8
imamyah <imanemachkour8@gmail.com>
2023-03-23 22:56:12 +0000

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,21 +3,21 @@
 /**
  * more_numbers - prints 10 times the numbers from 0 to 14
  * followed with a new line
- * Return: Always 0 (Success)
  */
 
 void more_numbers(void)
 {
 	int i, j;
 
-	j = 0;
-
-	while(j < 10)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i < 15; i++)
-			_putchar(i);
+		{
+			if(i >= 10)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
 		_putchar('\n');
-		j++;
 	}
 }
-
