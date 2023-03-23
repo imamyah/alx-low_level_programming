0e5321563801420c6884c02833e32fa7244e988b
imamyah <imanemachkour8@gmail.com>
2023-03-23 18:13:03 +0000

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	_putchat(c);
+	return (0);
+}
