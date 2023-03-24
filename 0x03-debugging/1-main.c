e7fe2ae754aaea1045f5767c0a1c8f960c041789
imamyah <imanemachkour8@gmail.com>
2023-03-24 22:01:09 +0000

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	/**while (i < 10)
 	{
-		putchar(i); /* No increment for the integer i, i is always 0*/
-	}
+		putchar(i);
+	}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
