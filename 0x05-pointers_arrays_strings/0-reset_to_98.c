cd85b8e0776e2784d03f2be2673b0441774f207d
imamyah <imanemachkour8@gmail.com>
2023-03-27 22:42:22 +0000

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,14 +3,10 @@
 /**
  * reset_to_98 -  updates an int to the value points to 98
  * @n: an integer to reset to 98
- * Return: Always 0 (Success)
+ * Return: n
  */
 
 void reset_to_98(int *n)
 {
-	int A[ 1 ] = {98};
-
-	n = A;
-
-	_putchar(*n);
+	*n = 98;
 }
