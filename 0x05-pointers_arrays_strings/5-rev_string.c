ce6f9e48f649b7748b6c5d5149b90a8725a10478
imamyah <imanemachkour8@gmail.com>
2023-03-28 02:04:20 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	int counter = 0;
+	char rev = s[0];
+
+	while (s[counter] != '\0')
+		counter++;
+	for (rev = counter; rev > 0; rev--)
+	{
+		_putchar(*s);
+		s--
+	}
+}
