b13bd102b3d504aa578c18ac7502b2d6a6d649c4
imamyah <imanemachkour8@gmail.com>
2023-03-28 16:30:14 +0000

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,14 @@
 void rev_string(char *s)
 {
 	int counter = 0;
-	char rev= s[0];
+	char rev;
 	int i;
 
 	while (s[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter; i++)
 	{
+		counter--;
 		rev = s[i];
 		s[i] = s[counter];
 		s[counter] = rev;
