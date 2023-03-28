2ce181c120f004d48adb22736fe4d540c34a5114
imamyah <imanemachkour8@gmail.com>
2023-03-28 22:30:52 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,9 +16,11 @@ void puts_half(char *str)
 		length++;
 	n = (length / 2);
 
-	if ((length % 2 == 1)
-			n = ((length -1) / 2);
+	if ((length % 2) == 1)
+		n = ((length -1) / 2);
+
 	for (i = n; str[i] != '\0'; i++)
-	_putchar(str[i]);
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
