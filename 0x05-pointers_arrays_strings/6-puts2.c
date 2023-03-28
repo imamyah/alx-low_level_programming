43bf182183a17733ba6b585bcec6338a23004ca1
imamyah <imanemachkour8@gmail.com>
2023-03-28 17:05:36 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting from the first character
+ * fllowed by a new line
+ * @str: The string
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+	for (i = 0; i <= length; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
