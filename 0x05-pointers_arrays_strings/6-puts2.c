d8c3c243c773171f34887b0182f435d12e7a9bb9
imamyah <imanemachkour8@gmail.com>
2023-03-28 17:54:17 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,15 +10,12 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
 	while (*(str + i) != '\0')
 	{
-		str++;
-	}
-	if (i % 2 == 0)
-	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
