cc432f184834031d1a54533e3dc70684ecbd2712
imamyah <imanemachkour8@gmail.com>
2023-03-28 01:08:55 +0000

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,18 @@
 
 /** _strlen - returns the length of a string
  * @s: The string
- * Return: s
+ * Return: length
  */
 
 int _strlen(char *s)
 {
 	int length = 0;
-	
+
 	while (*s != '\0')
 	{
 		length++;
 		s++;
 	}
 
-	return(length);
+	return (length);
 }
