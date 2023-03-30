21ef51d371d03ba063c1fc5698e354e1a36d476d
imamyah <imanemachkour8@gmail.com>
2023-03-30 22:29:09 +0000

    task 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,9 +13,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
+
 	for (; i > n; i++)
 		dest[i] = '\0';
+
 	return (dest);
 }
