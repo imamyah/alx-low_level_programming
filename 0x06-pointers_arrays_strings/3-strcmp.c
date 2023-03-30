a9fff9dbb3011ac715154940a90b652118de9422
imamyah <imanemachkour8@gmail.com>
2023-03-30 23:09:15 +0000

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,11 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1 == s2)
-			return (0);
-		else
+		if (s1 != s2)
+		{
 			return (s1 - s2);
+		}
 	}
+	return (0);
 }
 
