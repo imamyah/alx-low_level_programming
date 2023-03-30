deba132d239b9d42bdb2761501a8d7e960bb6816
imamyah <imanemachkour8@gmail.com>
2023-03-30 22:58:24 +0000

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1 == s2)
+			return (0);
+		else
+			return (s1 - s2);
+	}
+}
+
