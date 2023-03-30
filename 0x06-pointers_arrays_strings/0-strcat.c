5c000f27fc87195b4b34d7b47f0678daeefe4cb6
imamyah <imanemachkour8@gmail.com>
2023-03-30 17:39:33 +0000

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: copy from
+ * @dest: cpopy to
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int length = 0;
+	int i;
+
+	while (src[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	for (i = 0; i < length && src[i] != '\0'; i++)
+	{
+		dest[length] = src[i];
+	}
+	dest[length] = '\0';
+	return dest;
+}
