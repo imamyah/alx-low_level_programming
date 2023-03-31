11e3ce01934988ee14ffbdd8fdf59ce53176f3ac
imamyah <imanemachkour8@gmail.com>
2023-03-31 01:38:13 +0000

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,6 +23,11 @@ char *cap_string(char *str)
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] = str[i] - 32;
 		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		}
 	}
 	return (str);
 }
