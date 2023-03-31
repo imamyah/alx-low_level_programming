4a6ca096054889d1d65af8581ec0f5584a61acc2
imamyah <imanemachkour8@gmail.com>
2023-03-31 00:34:33 +0000

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: input value
+ *
+ * return: capitalized string
+ */
+
+char *cap_string(char *)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if(str[i] = 'a' && str[i] = 'z')
+				str[i] = str[i] - 32;
+		}
+		if (str[i] = ' ')
+		{
+			i++
+		}
+	}
+	return (0);
+}
