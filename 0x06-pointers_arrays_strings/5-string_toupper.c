4656e25b50246047ee7d3917eb331107190a46df
imamyah <imanemachkour8@gmail.com>
2023-03-31 00:09:37 +0000

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * @a: input value
+ *
+ * Return: string in uppercase
+ */
+
+char *string_toupper(char *)
+{
+	char a;
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+			a[i] = a[i] -32;
+	}
+	return (a);
+}
