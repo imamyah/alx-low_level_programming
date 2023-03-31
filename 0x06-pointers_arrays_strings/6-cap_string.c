7585312f1439b8b6d85c90067181a9d9a8a61cd1
imamyah <imanemachkour8@gmail.com>
2023-03-31 01:32:53 +0000

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,11 +18,11 @@ char *cap_string(char *str)
 			if(str[i] >= 'a' && str[i] <= 'z')
 				str[i] = str[i] - 32;
 		}
-		if (str[i] == ' '|| str[i] == '\t'|| str[i] == '\n'|| str[i] == ','|| str[i] == ';'|| str[i] == '?'|| str[i] == '!' || str[i] == '"'|| str[i] == '('|| str[i] == ')'|| str[i] == '{'|| str[i] == '}')
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '?' || str[i - 1] == '!' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] = str[i] - 32;
 		}
 	}
-	return (0);
+	return (str);
 }
