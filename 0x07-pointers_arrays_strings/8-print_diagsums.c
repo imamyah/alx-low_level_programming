97cad2b5597a6f42dfb8584afc3867d8398506d1
imamyah <imanemachkour8@gmail.com>
2023-04-04 23:45:05 +0000

    chessboard

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: input value
+ * @size: input value
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_diagsums(int *a, int size)
+{
+
