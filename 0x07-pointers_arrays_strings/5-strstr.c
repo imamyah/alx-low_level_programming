45ac2f80870d672c6812eafee2de68b979479442
imamyah <imanemachkour8@gmail.com>
2023-04-04 00:51:36 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,9 +15,9 @@ char *_strstr(char *haystack, char *needle)
 		while (*haystack == *needle && *needle != '\0')
 		{
 			haystack++;
-			needle;
+			needle++;
 		}
-		if (*needle = '\0')
+		if (*needle == '\0')
 			return (haystack);
 	}
 	return (0);
