684114e537212ad5134289854dd4076cffcb7d65
imamyah <imanemachkour8@gmail.com>
2023-04-05 21:30:00 +0000

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,9 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
+	if (*s == '\0')
+		_putchar('\n');
 
-	if (s[i] != '\0')
-		_putchar(s[i]);
-
-	i++;
-
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
