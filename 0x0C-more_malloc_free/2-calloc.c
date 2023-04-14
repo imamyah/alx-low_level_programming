b082af08e5a41658e0875b1bf7f69ce3455c2d35
imamyah <imanemachkour8@gmail.com>
2023-04-14 00:48:44 +0000

    quick modification

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb , unsigned int size)
 	char *ar;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
 
 	ar = malloc(size * nmemb);
