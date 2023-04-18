b42963eb36b159aac65250d407d2f10121ae3b91
imamyah <imanemachkour8@gmail.com>
2023-04-18 00:36:21 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog basic information
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}	
+};	
