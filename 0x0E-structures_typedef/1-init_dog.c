d759f11f512fb8959a35975d095d35cbd46b1076
imamyah <imanemachkour8@gmail.com>
2023-04-18 01:11:27 +0000

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = maloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
-	else
+
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
