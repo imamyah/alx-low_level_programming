a696143e8db3a45eec8f381caa6665ff307b1b55
imamyah <imanemachkour8@gmail.com>
2023-04-19 16:10:29 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: the string to add
+ * @f: the pointer to function
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void(*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+	else
+		return;
