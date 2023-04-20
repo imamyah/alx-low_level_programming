b46eb871916b35c900b4c25392ec7a8ee2bf7bb7
imamyah <imanemachkour8@gmail.com>
2023-04-20 08:18:52 -0700

    rewrite code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -9,7 +9,7 @@
  * Return: nothing
  */
 
-void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
