5fe5ed21dddbc7fb6a4adb04b8c7e334eb002543
imamyah <imanemachkour8@gmail.com>
2023-04-20 11:30:29 -0700

    add all the files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,4 +3,22 @@
 
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	}
+	int i = 0;
+
+	while (op_s[i].op)
+	{
+		if (*(op_s[i]).op == *s)
+			return (op_s[i].f);
+		i++;
+	}
+
+	return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,4 +3,60 @@
 
 /**
  * op_add - return the sum of two integers
+ * @a: first element
+ * @b: second element
  *
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return the difference of two integers
+ * @a: first element
+ * @b: second element
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - return the product of two integers
+ * @a: first element
+ * @b: second element
+ *
+ * Return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - return the division of two integers
+ * @a: the first element
+ * @b: the second element
+ *
+ * Return: the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return the remainder of the division of two integers
+ * @a: the first element
+ * @b: the second element
+ *
+ * Return: the remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
