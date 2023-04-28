6b6fb1fbb4398b6e23811a2cc817dc7ca5821edb
imamyah <imanemachkour8@gmail.com>
2023-04-28 23:22:26 +0000

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: the address of the node
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	while (!head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(temp);
+	}
+}
