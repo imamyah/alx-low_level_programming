e83e86950e17f243403329b17eb3ca4025b0d2b8
imamyah <imanemachkour8@gmail.com>
2023-04-28 19:52:50 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,13 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n, h->len, h->str");
+			printf("[%d] %s\n", h->len, h->str);
 		}
-		h->next;
-		*h->h;
+		h = h->next;
 		i++;
 	}
 	return (i);
