1d5f50a5303e30cab4074ce7f606ca8cbbf22c66
imamyah <imanemachkour8@gmail.com>
2023-05-01 18:31:19 +0000

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,13 @@
  * Return: the number of nodes
  */
 
-size_t print_listint(const listint *h)
+size_t print_listint(const listint_t *h)
 {
 	int count = 0;
 
 	while (h)
 	{
-		h->n;
-		h->next;
-		printf("%d\n%d\n", n, next);
+		printf("%i\n", h->n);
 		h = h->next;
 		count++;
 	}
