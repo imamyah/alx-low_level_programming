25cbec7e23d6ea78e814e82b2a628b0ca6f71710
imamyah <imanemachkour8@gmail.com>
2023-05-04 23:03:21 +0000

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,18 +12,15 @@ void print_binary(unsigned long int n)
 	int i, count = 0;
 	unsigned long int mask;
 
-	/**assuming 64-bit integer*/
 	for (i = 63; i >= 0; i--)
 	{
-		/**calculate bitmask to check whether
-		 * ith bit of n is set or not*/
 		mask = n >> i;
 
 		if (mask & 1)
 		{
 			_putchar('1');
 			count++;
-		else if (count)
+		} else if (count)
 			_putchar('0');
 	}
 	if (!count)
