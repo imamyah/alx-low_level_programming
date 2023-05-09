4010e0ee153bf70b363c5992134de8fd546ce61b
imamyah <imanemachkour8@gmail.com>
2023-05-09 00:40:47 +0000

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on Success
+ */
+
+int main(int argc, char *argv[])
+{
+}
