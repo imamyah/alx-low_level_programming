2ccebd72a6a6cbf803a76d59d93ba8bf8b41c18c
imamyah <imanemachkour8@gmail.com>
2023-05-09 00:31:13 +0000

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 	}
-	fd = open(filename, O_RDONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	wr = write(fd, text_content, len);
 
 	if (fd == -1 || wr == -1)
