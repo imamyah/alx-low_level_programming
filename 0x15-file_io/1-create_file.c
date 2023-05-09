4dbd508da59c32ad0d0a0380cb458903ca9c0a0e
imamyah <imanemachkour8@gmail.com>
2023-05-09 00:06:40 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: the NULL terminted string to write the file
+ *
+ * Return: 1 on Success, -1 on Failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, len = 0;
+
+	if (filename == NULL)
+		retun (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+	fd = open(filename, o_CREATE | O_RDWR | O_TRUNC, 0600);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}	
